e61fdff886e39e6ac2f8b2ecd134897e095b59ef
zydnet <devanshi.ag1@gmail.com>
2025-11-19 14:59:18 +0530

    login screen added

--- CineMystApp/Login/LoginViewController.swift
+++ CineMystApp/Login/LoginViewController.swift
@@ -9,12 +9,52 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
-    }
-
+    
+    
+    applyGradientBackground()
+      }
+      
+      override func viewDidLayoutSubviews() {
+          super.viewDidLayoutSubviews()
+          // ensure gradient resizes on rotation or layout changes
+          updateGradientFrame()
+      }
+      
+      // MARK: - Gradient Layer
+      
+      private var gradientLayer: CAGradientLayer?
+      
+      private func applyGradientBackground() {
+          let gradient = CAGradientLayer()
+          
+          gradient.colors = [
+            UIColor(red: 54/255, green: 18/255, blue: 52/255, alpha: 1).cgColor, // top color
+            UIColor(red: 22/255, green: 8/255, blue: 35/255, alpha: 1).cgColor   // bottom color
+          ]
+          gradient.startPoint = CGPoint(x: 0, y: 0)
+          gradient.endPoint = CGPoint(x: 1, y: 1)
+          gradient.frame = view.bounds
+          
+          // Clean old layers if you hot-reload
+          view.layer.sublayers?
+              .filter { $0 is CAGradientLayer }
+              .forEach { $0.removeFromSuperlayer() }
+          
+          view.layer.insertSublayer(gradient, at: 0)
+          gradientLayer = gradient
+      }
+      
+      private func updateGradientFrame() {
+          gradientLayer?.frame = view.bounds
+      }
+    
+    
 
     /*
     // MARK: - Navigation
